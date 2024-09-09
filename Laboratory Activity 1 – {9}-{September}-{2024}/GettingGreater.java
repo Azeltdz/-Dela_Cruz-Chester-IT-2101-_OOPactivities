9cd79de39f0c9db5f91d2041ef1a6d7b69a2080c
Chester Paul D. Dela Cruz <azeltdeuz@gmail.com>
2024-09-09 11:18:29 +0800

    Add files via upload

---
+++ Laboratory Activity 1 – {9}-{September}-{2024}/GettingGreater.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GettingGreater {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        System.out.println("Getting the Greater Value");
+        System.out.print("Enter first character: ");
+        char first = value.next().charAt(0);
+
+        System.out.print("Enter second character: ");
+        char second = value.next().charAt(0);
+
+        char maxChar = (char) Math.max(first, second);
+
+        System.out.println("------------------------------");
+        System.out.println("The character with greater value is: " + maxChar);
+        System.out.println("------------------------------");
+
+        System.out.println("Showing the ASCII Codes");
+        System.out.println(first + " : " + (int)first);
+        System.out.println(second + " : " +  (int)second);
+
+        value.close();
+    }
+}
